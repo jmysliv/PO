46f7923b2664ccbda8825d6a3b86e16be8c04582
jmysliv <jjmreal34@gmail.com>
2019-10-09 21:40:20 +0200

    fixed tests

--- lab1/src/main/agh/cs/lab2/Vector2d.java
+++ lab1/src/main/agh/cs/lab2/Vector2d.java
@@ -5,60 +5,47 @@ public class Vector2d {
     public final int x, y;
 
     public Vector2d(int x, int y){
-        this.x=x;
-        this.y=y;
+        this.x = x;
+        this.y = y;
     }
 
     public String toString(){
-        String result = "(" + Integer.toString(this.x) + "," + Integer.toString(this.y) + ")" ;
-        return result;
+        return String.format("(%s, %s)", x, y);
     }
 
-    public boolean smaller(Vector2d other){
-        if(this.x<=other.x && this.y<=other.y) return true;
-        return false;
+    public boolean precedes(Vector2d other){
+        return this.x <= other.x && this.y <= other.y;
     }
 
-    public boolean larger(Vector2d other){
-        if(this.x>=other.x && this.y>=other.y) return true;
-        return false;
+    public boolean follows(Vector2d other){
+        return this.x >= other.x && this.y >= other.y;
     }
 
     public Vector2d upperRight(Vector2d other){
-        int x, y;
-        if(this.x > other.x) x=this.x;
-        else x=other.x;
-
-        if(this.y > other.y) y=this.y;
-        else y=other.y;
-
-        Vector2d result = new Vector2d(x, y);
-        return result;
+        return new Vector2d(Math.max(x, other.x), Math.max(y, other.y));
     }
 
     public Vector2d lowerLeft(Vector2d other){
-        int x, y;
-        if(this.x < other.x) x=this.x;
-        else x=other.x;
-
-        if(this.y < other.y) y=this.y;
-        else y=other.y;
-
-        Vector2d result = new Vector2d(x, y);
-        return result;
+        return new Vector2d(Math.min(x, other.x), Math.min(y, other.y));
     }
 
     public Vector2d add(Vector2d other){
-        Vector2d result = new Vector2d(this.x + other.x, this.y + other.y);
-        return result;
+        return new Vector2d(this.x + other.x, this.y + other.y);
     }
 
     public Vector2d substract(Vector2d other){
-        Vector2d result = new Vector2d(this.x - other.x, this.y - other.y);
-        return result;
+        return new Vector2d(this.x - other.x, this.y - other.y);
     }
 
-    boolean equals(Vector2d other){
-        return (this.larger(other) && this.smaller(other));
+    public boolean equals(Object other) {
+        if(this == other) return true;
+        if(!(other instanceof Vector2d)) return false;
+
+        Vector2d v = (Vector2d)other;
+        return x == v.x && y == v.y;
+    }
+
+    public Vector2d oposite(){
+        return new Vector2d( - this.x, - this.y);
     }
 }
---
+++ lab1/src/test/agh/cs/lab2/Vector2dTest.java
@@ -0,0 +1,44 @@
+package agh.cs.lab2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Vector2dTest {
+
+    @Test
+    public void toString() {
+    }
+
+    @Test
+    public void precedes() {
+    }
+
+    @Test
+    public void follows() {
+    }
+
+    @Test
+    public void upperRight() {
+    }
+
+    @Test
+    public void lowerLeft() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void substract() {
+    }
+
+    @Test
+    public void equals() {
+    }
+
+    @Test
+    public void oposite() {
+    }
+}
