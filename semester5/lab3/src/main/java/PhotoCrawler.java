8ea62232631e277aab108f309ba030ccaa9d8dda
jmysliv <jjmreal34@gmail.com>
2020-10-30 15:29:55 +0100

    Added lab3

---
+++ semester5/lab3/src/main/java/PhotoCrawler.java
@@ -0,0 +1,70 @@
+import io.reactivex.rxjava3.core.Observable;
+import model.Photo;
+import util.PhotoDownloader;
+import util.PhotoProcessor;
+import util.PhotoSerializer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PhotoCrawler {
+
+    private static final Logger log = Logger.getLogger(PhotoCrawler.class.getName());
+
+    private final PhotoDownloader photoDownloader;
+
+    private final PhotoSerializer photoSerializer;
+
+    private final PhotoProcessor photoProcessor;
+
+    public PhotoCrawler() throws IOException {
+        this.photoDownloader = new PhotoDownloader();
+        this.photoSerializer = new PhotoSerializer("./photos");
+        this.photoProcessor = new PhotoProcessor();
+    }
+
+    public void resetLibrary() throws IOException {
+        photoSerializer.deleteLibraryContents();
+    }
+
+    public void downloadPhotoExamples() {
+        try {
+            var downloadedExamples = photoDownloader
+                    .getPhotoExamples()
+                    .compose(this::processPhoto);
+            downloadedExamples.subscribe(photoSerializer::savePhoto);
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Downloading photo examples error", e);
+        }
+    }
+
+    public void downloadPhotosForQuery(String query) throws IOException {
+        try {
+            var downloadedExamples = photoDownloader
+                    .searchForPhotos(query)
+                    .compose(this::processPhoto);
+            downloadedExamples.subscribe(photoSerializer::savePhoto);
+        } catch (IOException | InterruptedException e) {
+            log.log(Level.SEVERE, "Downloading photo examples error", e);
+        }
+    }
+
+    public void downloadPhotosForMultipleQueries(List<String> queries){
+        try {
+            var downloadedExamples = photoDownloader
+                    .searchForPhotos(queries)
+                    .compose(this::processPhoto);
+            downloadedExamples.subscribe(photoSerializer::savePhoto);
+        } catch (IOException | InterruptedException e) {
+            log.log(Level.SEVERE, "Downloading photo examples error", e);
+        }
+    }
+
+    public Observable<Photo> processPhoto(Observable<Photo> obsrvables){
+        return obsrvables
+                .filter(photoProcessor::isPhotoValid)
+                .map(photoProcessor::convertToMiniature);
+    }
+}
